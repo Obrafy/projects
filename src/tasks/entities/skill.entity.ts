b931684d41dab3a4bb86a44b475fa7095b315be8
NicholasNeto <nicholasneto@outlook.com>
2022-04-26 23:36:58 -0300

    created new entity Skill

---
+++ src/tasks/entities/skill.entity.ts
@@ -0,0 +1,13 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from 'mongoose';
+
+export type SkillDocument = Skill & Document;
+
+@Schema()
+export class Skill {
+
+    @Prop()
+    name: string; 
+}
+export const SkillSchema = SchemaFactory.createForClass(Skill);
+
