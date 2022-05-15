3647b504cbd5714af07513ab80119f8e08912d71
NicholasNeto <nicholasneto@outlook.com>
2022-05-14 21:46:31 -0300

    fix projectTasks in project change for array

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,55 +1,38 @@
-import {  Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
-// import { Document, SchemaTypes, Types} from 'mongoose';
+import mongoose, { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Address } from './address.entity';
 import { ProjectTasks } from './projectTasks.entity';
-import { Document, Schema as MongooseSchema } from 'mongoose';
-
 
 enum StatusType {
-    START = 'start',
-    FINISHED='finished', 
-    IN_PROGRESS='in-progress',
-  }
+  START = 'start',
+  FINISHED = 'finished',
+  IN_PROGRESS = 'in-progress',
+}
 
 export type ProjectDocument = Project & Document;
 
 @Schema()
 export class Project {
+  @Prop({ type: String, enum: StatusType, default: StatusType.START })
+  status: string;
 
-    @Prop({ type: String, enum: StatusType, default: StatusType.START })
-    status: string;
+  @Prop({ type: Date, default: Date.now })
+  startDate: Date;
 
-    @Prop({ type: Date, default: Date.now })
-    startDate: Date;
+  @Prop({ type: Date })
+  endDate: Date;
 
-    @Prop({ type: Date })
-    endDate: Date;
+  @Prop({ type: Date })
+  expectedFinishedDate: Date;
 
-    @Prop({ type: Date })
-    expectedFinishedDate: Date;
+  @Prop()
+  responsible: string;
 
-    @Prop()
-    responsible: string;
+  @Prop({ type: mongoose.Types.ObjectId, ref: 'Address' })
+  address: Address;
 
-    @Prop({ type: mongoose.Types.ObjectId, ref: 'Address' })
-    address: Address; 
-
-    @Prop({ type: ProjectTasks })
-    tasks: ProjectTasks[];  
+  @Prop([ProjectTasks])
+  tasks: ProjectTasks[];
 }
 
 export const ProjectSchema = SchemaFactory.createForClass(Project);
-
-
-// // Parent Schema
-// @Schema()
-// export class ChaptersApi extends Document {
-//   // Array example
-//   @Prop({ type: [BodySchema], default: [] })
-//   body: BodyContentInterface[];
-
-//   // Single example
-  
-//   body: BodyContentInterface;
-// }
--- src/projects/entities/projectTasks.entity.ts
+++ src/projects/entities/projectTasks.entity.ts
@@ -1,18 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import { Document } from 'mongoose';
-import { Prop, raw, Schema } from "@nestjs/mongoose";
-import { Task } from "src/tasks/entities/task.entity";
+import { Prop, raw, Schema } from '@nestjs/mongoose';
+import { Task } from 'src/tasks/entities/task.entity';
 @Schema({ _id: false })
 export class ProjectTasks extends Document {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Task' })
+  task: Task;
 
-    @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }] })
-    task: Task;
-
-    @Prop(raw({
-        category: { type: String },
-        activity: { type: String },
-        noiseLevel: { type: String },
-        dirtyLevel: { type: String },
-    }))
-    fieldsOverwriters: Record<string, any>;
+  @Prop(
+    raw({
+      category: { type: String },
+      activity: { type: String },
+      noiseLevel: { type: String },
+      dirtyLevel: { type: String },
+    }),
+  )
+  fieldsOverwriters: Record<string, any>;
 }
