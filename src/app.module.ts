24fbfac369c37971647d308bb1cd7fe7d528d10d
NicholasNeto <nicholasneto@outlook.com>
2022-05-24 23:59:29 -0300

    removed ClientsModule.register

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
-import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { ProjectsModule } from './projects/projects.module';
 import { ConfigInterface, loader, validationSchema } from './config';
 import { TasksModule } from './tasks/tasks.module';
-import { AUTH_PACKAGE_NAME, protobufPackage } from './common/proto/auth.pb';
 
 @Module({
   imports: [
@@ -31,23 +28,6 @@ import { AUTH_PACKAGE_NAME, protobufPackage } from './common/proto/auth.pb';
     ProjectsModule,
 
     TasksModule,
-
-    ClientsModule.register([
-      {
-        name: AUTH_PACKAGE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          package: protobufPackage,
-          protoPath: join(
-            'node_modules',
-            'proto',
-            'proto-files',
-            'authentication-service',
-            'auth.proto',
-          ),
-        },
-      },
-    ]),
   ],
   controllers: [AppController],
   providers: [],
