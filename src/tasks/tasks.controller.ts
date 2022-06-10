1acc864d9b68ee9011610ba7476a7ce0361eb91f
NicholasNeto <nicholasneto@outlook.com>
2022-06-10 02:05:16 -0300

    ajusted Task for response

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -5,6 +5,7 @@ import * as PROTO from '../common/dto/proto/project.pb';
 import * as DTO from './dto/task.dto';
 import makeResponse from 'src/common/helpers/make-response';
 import { Status } from 'src/common/dto/status.enum';
+
 @Controller()
 export class TasksController {
   @Inject(TasksService)
@@ -12,38 +13,38 @@ export class TasksController {
 
   @GrpcMethod(PROTO.TASK_SERVICE_NAME, 'Create')
   private async create(createTaskDto: DTO.TaskCreateRequestDto): Promise<PROTO.TaskCreateResponse> {
-    const taskData = await this.tasksService.create(createTaskDto);
+    const task = await this.tasksService.create(createTaskDto);
 
     return makeResponse<PROTO.TaskCreateResponse>({
-      task: {
-        category: taskData.category,
-        activity: taskData.activity,
-        noiseLevel: taskData.noiseLevel,
-        dirtLevel: taskData.dirtLevel,
-        description: taskData.description,
-        unity: taskData.unity,
-        possibleSkills: taskData.possibleSkills,
-        status: taskData.status,
-      },
+      category: task.category,
+      activity: task.activity,
+      noiseLevel: task.noiseLevel,
+      dirtLevel: task.dirtLevel,
+      description: task.description,
+      unity: task.unity,
+      possibleSkills: task.possibleSkills,
+      status: task.status,
+      id: task._id,
+      priority: task.priority
     });
   }
 
   @GrpcMethod(PROTO.TASK_SERVICE_NAME, 'FindAll')
   private async findAll(): Promise<PROTO.TaskFindAllResponse> {
-    const tasksData = await this.tasksService.findAll();
+    const task = await this.tasksService.findAll();
 
-    const result = tasksData.map((task) => {
+    const result = task.map((task) => {
       return {
-        task: {
-          category: task.category,
-          activity: task.activity,
-          noiseLevel: task.noiseLevel,
-          dirtLevel: task.dirtLevel,
-          description: task.description,
-          unity: task.unity,
-          possibleSkills: task.possibleSkills,
-          status: task.status,
-        },
+        category: task.category,
+        activity: task.activity,
+        noiseLevel: task.noiseLevel,
+        dirtLevel: task.dirtLevel,
+        description: task.description,
+        unity: task.unity,
+        possibleSkills: task.possibleSkills,
+        status: task.status,
+        id: task._id,
+        priority: task.priority
       };
     });
 
@@ -52,40 +53,40 @@ export class TasksController {
 
   @GrpcMethod(PROTO.TASK_SERVICE_NAME, 'FindOne')
   private async findOne(payload: DTO.TaskFindOneRequestDto): Promise<PROTO.TaskFindOneResponse> {
-    const taskData = await this.tasksService.findOne(payload);
+    const task = await this.tasksService.findOne(payload);
 
     return makeResponse<PROTO.TaskFindOneResponse>({
-      task: {
-        category: taskData.category,
-        activity: taskData.activity,
-        noiseLevel: taskData.noiseLevel,
-        dirtLevel: taskData.dirtLevel,
-        description: taskData.description,
-        unity: taskData.unity,
-        possibleSkills: taskData.possibleSkills,
-        status: taskData.status,
-      },
+      category: task.category,
+      activity: task.activity,
+      noiseLevel: task.noiseLevel,
+      dirtLevel: task.dirtLevel,
+      description: task.description,
+      unity: task.unity,
+      possibleSkills: task.possibleSkills,
+      status: task.status,
+      id: task._id,
+      priority: task.priority
     });
   }
 
   @GrpcMethod(PROTO.TASK_SERVICE_NAME, 'Update')
   private async update({ taskId, data }: DTO.TaskUpdateRequestDto): Promise<PROTO.TaskUpdateResponse> {
-    const taskData = await this.tasksService.update({
+    const task = await this.tasksService.update({
       taskId,
       data: data as PROTO.TaskUpdateData,
     });
 
     return makeResponse<PROTO.TaskFindOneResponse>({
-      task: {
-        category: taskData.category,
-        activity: taskData.activity,
-        noiseLevel: taskData.noiseLevel,
-        dirtLevel: taskData.dirtLevel,
-        description: taskData.description,
-        unity: taskData.unity,
-        possibleSkills: taskData.possibleSkills,
-        status: taskData.status,
-      },
+      category: task.category,
+      activity: task.activity,
+      noiseLevel: task.noiseLevel,
+      dirtLevel: task.dirtLevel,
+      description: task.description,
+      unity: task.unity,
+      possibleSkills: task.possibleSkills,
+      status: task.status,
+      id: task._id,
+      priority: task.priority
     });
   }
 
